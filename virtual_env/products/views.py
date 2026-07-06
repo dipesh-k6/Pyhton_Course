ec0bc169aa203bdc9e0d46ab106882a4650bbc57
Dipesh <dipeshpc1995@gmail.com>
2026-07-06 07:41:14 +0545

    using templates with context

--- virtual_env/products/views.py
+++ virtual_env/products/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def test(request):
     return HttpResponse("this is index page")
+
+def index(request):
+    data = {
+        "name": "Adam",
+        "age": 25
+    }
+    return render(request, 'products/index.html',context = data)
